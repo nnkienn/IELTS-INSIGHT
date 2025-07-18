6c80af414bd9182210a63d6eab10ffa8be6dd4ea
nnkienn <kientrungng.2002@gmail.com>
2025-07-18 23:57:39 +0700

    7/18

--- scoring-backend-microservice/user-service/src/main.ts
+++ scoring-backend-microservice/user-service/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// src/main.ts
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+  const port = process.env.USER_SERVICE_PORT || 3001;
+  await app.listen(port, '0.0.0.0');   // thêm '0.0.0.0' ở đây
+  console.log(`Listening on 0.0.0.0:${port}`);
 }
 bootstrap();
