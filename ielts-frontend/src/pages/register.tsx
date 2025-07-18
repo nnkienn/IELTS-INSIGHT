4d11d67f3dafa866b5fd71e4193e84c5fd3ef2e5
nnkienn <kientrungng.2002@gmail.com>
2025-07-18 10:32:15 +0700

    7/18

--- ielts-frontend/src/pages/register.tsx
+++ ielts-frontend/src/pages/register.tsx
@@ -5,46 +5,47 @@ import { useRouter } from "next/router"
 import { useState } from "react"
 import { v4 as uuidv4 } from 'uuid'
 
-export default function registerPage() {
+export default function RegisterPage() {
     const dispatch = useAppDispatch()
     const router = useRouter()
     const [username, setUsername] = useState<string>('')
     const [email, setEmail] = useState<string>('')
-    const [role , setRole] = useState<Role>('student')
+    const [role, setRole] = useState<Role>('student')
 
-    const handleSubmit = (e : React.FormEvent)=>{
+    const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault()
-        if(!username.trim() || !email.trim()){
+        if (!username.trim() || !email.trim()) {
             alert("Please type Email & username")
             return
         }
-        const newUser : User ={
-            id : uuidv4(),
-            email : email,
-            username : username,
-            role : role,
-            createdAt : new Date().toISOString()
+        const newUser: User = {
+            id: uuidv4(),
+            email: email,
+            username: username,
+            role: role,
+            createdAt: new Date().toISOString()
         }
         dispatch(login(newUser))
-        localStorage.setItem('user',JSON.stringify(newUser))
+        localStorage.setItem('user', JSON.stringify(newUser))
         router.push('/dashboard')
 
 
 
     }
 
-    return(
-        <div>
-            <form onSubmit={handleSubmit}>
-                <input type="email" name="" id="" placeholder="Your email" onChange={e=>setEmail(e.target.value)}/>
-                <input type="text" name="" id="" placeholder="Your username" onChange={e=>setUsername(e.target.value)}/>
-                <select name="" id="" value={role} onChange={e=>setRole(e.target.value as Role)}>
-                    <option value="student">student</option>
-                    <option value="teacher">teacher</option>
-                </select>
-                <button type="submit">Register</button>
-            </form>
-        
+    return (
+        <div className="min-h-screen flex items-center justify-center bg-[#2E2B3A] px-4 py-8">
+            <div className="flex w-full max-w-6xl bg-[#1F1D2B] rounded-2xl overflow-hidden shadow-2xl md:flex-row flex-col" >
+                {/* <form onSubmit={handleSubmit}>
+                    <input type="email" name="" id="" placeholder="Your email" onChange={e => setEmail(e.target.value)} />
+                    <input type="text" name="" id="" placeholder="Your username" onChange={e => setUsername(e.target.value)} />
+                    <select name="" id="" value={role} onChange={e => setRole(e.target.value as Role)}>
+                        <option value="student">student</option>
+                        <option value="teacher">teacher</option>
+                    </select>
+                    <button type="submit">Register</button>
+                </form> */}
+            </div>
         </div>
     )
 }
